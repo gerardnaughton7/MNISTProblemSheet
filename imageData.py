9cb68d81b71e378b4709eca99b33a37b902ccfb1
gerard naughton <gerard naughton>
2017-09-21 17:12:07 +0100

    unzipping images and reading in image per byte

---
+++ imageData.py
@@ -0,0 +1,9 @@
+# MNIST problem sheet
+# Gerard Naughton G00209309
+
+# Part 1 : Download the image and label files, have Python decompress them and read them byte by byte into appropriate data structures in memory.
+
+# Adapted code discovered on this link
+#https://stackoverflow.com/questions/12902540/read-from-a-gzip-file-in-python
+
+#import gzip
